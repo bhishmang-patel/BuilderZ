b53b52776599552ea1721c6be00c00972d9d0e99
Bhishmang Patel <bhishmang.patel@example.com>
2026-01-28 13:26:08 +0530

    chore: cleanup unused debug and temporary files

--- check_schema.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-$db = Database::getInstance();
-$cols = $db->query('SHOW COLUMNS FROM bookings')->fetchAll(PDO::FETCH_COLUMN);
-print_r($cols);
--- inspect_flats_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-$db = Database::getInstance();
-$cols = $db->query("DESCRIBE flats")->fetchAll(PDO::FETCH_ASSOC);
-foreach ($cols as $col) {
-    echo $col['Field'] . " (" . $col['Type'] . ")\n";
-}
